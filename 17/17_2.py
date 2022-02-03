292eab3761eb25b8717a9ded22015db4e2acae69
taratenkoa <andrey56634@gmail.com>
2022-02-03 19:11:15 +0300

    lesson_17 (2)

--- 17/17_2.py
+++ 17/17_2.py
@@ -1,4 +1,3 @@
-#пока нема
 def split_expression(s, sign):
     schet = 0
     L = [False, None, None, None]
@@ -7,9 +6,9 @@ def split_expression(s, sign):
         kol -= 1
         if i == ')':
             schet += 1
-        if i == '(':
+        elif i == '(':
             schet -= 1
-        if schet == 0 and i in sign and kol != -len(s) - 1 and s[kol] != 'e':
+        elif schet == 0 and i in sign and kol != -len(s) - 1 and s[kol] != 'e':
             L[0] = True
             L[1] = i
             L[2] = s[:kol + 1]
@@ -18,5 +17,39 @@ def split_expression(s, sign):
     return L
 
 
+def calc_expression(a):
+    return expr(a)
 
-def calc_expression(s):
+
+def expr(a):
+    L = split_expression(a, '+-')
+    if L[0]:
+        if L[1] == '+':
+            return expr(L[2]) + item(L[3])
+        else:
+            return expr(L[2]) - item(L[3])
+    else:
+        return item(a)
+
+
+def item(a):
+    L = split_expression(a, '*/')
+    if L[0]:
+        if L[1] == '*':
+            return item(L[2]) * factor(L[3])
+        else:
+            return item(L[2]) / factor(L[3])
+    else:
+        return factor(a)
+
+
+def factor(a):
+    if a[0] == '-':
+        return float(factor(a[1:]) * -1)
+    elif a[0] == '+':
+        return float(factor(a[1:]) * 1)
+    elif a[0] == '(':
+        return expr(a[1:-1])
+    else:
+        return float(a)
+
